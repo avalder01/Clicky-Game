3c52184090f764a207e00f7beb9a386d7500ce70
Allie Valder <avalder19@gmail.com>
2019-03-02 12:45:41 -0500

    working clicky game

---
+++ src/components/Header/index.js
@@ -0,0 +1,9 @@
+import React from "react";
+import "./Header.css";
+
+function Header(props) {
+    return <div className="header">
+    <h1>Clicky-Game</h1>
+    <h3>Don't click the same picture twice!</h3></div>;
+}
+export default Header;
